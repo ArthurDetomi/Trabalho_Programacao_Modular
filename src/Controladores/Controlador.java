848e686c95ad89bc5a55bfc6d5660a542d4e335e
Arthur Detomi <arthurdetomi@gmail.com>
2023-10-18 18:14:16 -0300

    Atualizando classe Controlador

--- src/Controladores/Controlador.java
+++ src/Controladores/Controlador.java
@@ -1,6 +1,12 @@
 package Controladores;
 
+import Robos.Direcoes;
+import Robos.Movimentacao;
 import Robos.Robo;
+import Terrenos.Posicao;
+import Terrenos.Terreno;
+
+import java.util.Map;
 
 public class Controlador {
 
@@ -32,4 +38,25 @@ public class Controlador {
     public void setRobo(Robo robo) {
         this.robo = robo;
     }
+
+    public Posicao getPosicaoAtualRobo() {
+        return robo.getPosicaoAtual();
+    }
+
+    public double getConcentracaoHelioPosicaoAtualRobo() {
+        return robo.getConcentracaoHelioPosicaoAtual();
+    }
+
+    public Map<Direcoes, Double> getRugosidadeRegiao(Terreno terreno) {
+        return robo.getRugosidadeRegiao(terreno);
+    }
+
+    public long getTempoDecorridoProspeccao() {
+        return robo.getTempoDecorridoMillis();
+    }
+
+    public boolean movimentarRobo(Movimentacao movimento, Terreno terreno) {
+        return robo.movimentar(movimento, terreno);
+    }
+
 }
