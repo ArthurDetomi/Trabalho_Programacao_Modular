8f51701ef7aa114f7c13e70e24a9e64418de64c1
Arthur Detomi <arthurdetomi@gmail.com>
2023-10-20 21:35:40 -0300

    Modo de implementar outras estratégias

---
+++ src/main/java/edu/ufsj/trabalho/controladores/ControladorSegundo.java
@@ -0,0 +1,11 @@
+package edu.ufsj.trabalho.controladores;
+
+import edu.ufsj.trabalho.terrenos.Terreno;
+
+public class ControladorSegundo extends Controlador{
+    @Override
+    public void iniciarEstrategia(Terreno terreno) {
+        super.iniciarEstrategia(terreno);
+    }
+
+}
---
+++ src/main/java/edu/ufsj/trabalho/controladores/ControladorTerceiro.java
@@ -0,0 +1,11 @@
+package edu.ufsj.trabalho.controladores;
+
+import edu.ufsj.trabalho.terrenos.Terreno;
+
+public class ControladorTerceiro extends Controlador {
+    @Override
+    public void iniciarEstrategia(Terreno terreno) {
+        super.iniciarEstrategia(terreno);
+    }
+
+}
