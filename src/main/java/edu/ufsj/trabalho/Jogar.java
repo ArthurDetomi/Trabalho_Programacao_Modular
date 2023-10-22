c8c51300415bca640920171192e21cfa538c378a
Diogo-Honorato <diogoaugustomh@gmail.com>
2023-10-22 01:18:04 -0300

    Teste de multiplos robos

--- src/main/java/edu/ufsj/trabalho/Jogar.java
+++ src/main/java/edu/ufsj/trabalho/Jogar.java
@@ -12,19 +12,14 @@ public class Jogar {
     public static void main(String[] args) {
         // Primeiro ler o arquivo
         // Segundo inicializar o terreno e os robo
-        Controlador controlador = new Controlador();
-
-        Controlador controlador2 = new Controlador();
 
         Terreno terreno = new Terreno(4, 4);
 
-        Celula celulaPouso = terreno.getCelulaPosicao(new Posicao(0, 0));
 
         Controlador controlador = new Controlador();
         Robo robo = new Robo(controlador, terreno.getCelulaPosicao(new Posicao(0,0)), Direcoes.DIREITA);
 
-        Robo robo2 = new Robo(controlador2, terreno.getCelulaPosicao(new Posicao(1, 0)), Direcoes.ESQUERDA);
-        controlador2.setRobo(robo2);
+
         // Iniciar loop do jogo
         int duracaoTotal = 180;
 
@@ -33,9 +28,6 @@ public class Jogar {
         while(duracaoTotal > 0) {
             controlador.iniciarEstrategia(terreno);
             robo.sinalizarTempoPassado();
-
-            controlador2.iniciarEstrategia(terreno);
-            robo2.sinalizarTempoPassado();
 
             duracaoTotal--;
         }
